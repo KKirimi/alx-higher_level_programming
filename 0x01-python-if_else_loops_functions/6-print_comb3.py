3a09760ea5e67740ec946a2085e9f6ce30ad516e
KKirimi <keithkirimi.kk@gmail.com>
2023-11-29 09:48:39 -0800

    Loops_Functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-for k in range(9):
-    for l in range(k + 1, 10):
-        if k * 10 + l < 89:
-            print("{:d}{:d}".format(k, l), end=", ")
-print("{:d}".format(89))
+# 6-print_comb3.py
+
+"""Print all possible different combinations of two digits in ascending order.
+
+    The two digits must be different - 01 and 10 are considered identical.
+    """
+for number1 in range(0, 10):
+    for number2 in range(number1 + 1, 10):
+        if number1 == 8 and number2 == 9:
+            print("{}{}".format(number1, number2))
+        else:
+            print("{}{}".format(number1, number2), end=", ")
