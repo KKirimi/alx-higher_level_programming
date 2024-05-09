5ae1ee0cff08c4762ea15930dc714f0bf37625ff
KKirimi <keithkirimi.kk@gmail.com>
2024-05-09 11:06:18 +0000

    0x05. Python - Exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_function(fct, *args):
+    result = None
     try:
         result = fct(*args)
-        return result
-    except Exception as k:
-        print("Exception:", k, file=sys.stderr)
-        return None
+    except Exception as error:
+        sys.stderr.write("Exception: {}\n".format(error))
+    return result
