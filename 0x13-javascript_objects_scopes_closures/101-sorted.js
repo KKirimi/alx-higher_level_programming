a2f214f3f264089d2977be8d19518de1814d839b
KKirimi <keithkirimi.kk@gmail.com>
2024-06-07 11:17:53 +0000

    0x13. JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,11 +3,11 @@
 const dict = require('./101-data').dict;
 const newDict = {};
 
-for (const key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = (key);
-  } else {
-    newDict[dict[key]].push(key);
+Object.keys(dict).map(function (key) {
+  if (!Array.isArray(newDict[dict[key]])) {
+    newDict[dict[key]] = [];
   }
-}
+  newDict[dict[key]].push(key);
+});
+
 console.log(newDict);
